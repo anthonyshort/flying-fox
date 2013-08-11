6adf3e38a19903b11bb3b87548e77c17f521d34a
Anthony Short <antshort+github@gmail.com>
2013-08-11 14:58:58 +1000

    Got the location JS working

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,67 +1,55 @@
-var status = document.querySelector('.js-status');
-var moving = false;
-var start;
-var stop;
+var startButton = document.querySelector('.js-get-location');
+var logEl = document.querySelector('.log');
 
-function page(n) {
-  document.body.classList.add('page-' + n);
-}
-
-function isMoving(motion) {
-  return true;
-}
-
-function showSuccess(start, end) {
-  document.body.classList.add('.page-success');
-
-}
-
-function showDistance() {
-
-}
-
-function distance(a_lat, a_lon, b_lat, b_lon) {
+function calculateDistance(a_lat, a_lon, b_lat, b_lon) {
   var R = 6371; // km
   return Math.acos(Math.sin(a_lat)*Math.sin(b_lat) +
                    Math.cos(a_lat)*Math.cos(b_lat) *
                    Math.cos(b_lon-a_lon)) * R;
 }
 
-document.querySelector('.js-get-location').addEventListener('click', function(event){
-  status.innerHTML = "Loading...";
-  event.preventDefault();
+function log(msg) {
+  logEl.innerHTML = logEl.innerHTML + "\n" + msg;
+}
 
-
-  navigator.geolocation.getCurrentPosition(function(startPosition){
-
-    window.addEventListener('devicemotion', function onMotion(motion){
-      if( isMoving(motion) ) {
-        moving = true
-      }
-      else if(moving === true) {
-        navigator.geolocation.getCurrentPosition(function(endPosition){
-          stop = endPosition;
-          window.removeEventListener('devicemotion', onMotion);
-          showSuccess(startPosition, endPosition);
-        });
-      }
-    });
-
+function getLocation(fn){
+  navigator.geolocation.getCurrentPosition(function(position){
+    fn(null, position);
   }, function(err){
-    status.innerHTML = err.message;
+    fn(err);
+  },{
+    enableHighAccuracy: true
+  });
+}
+
+startButton.addEventListener('click', function(event){
+  // show page 2
+  fly(function(err, distance){
+    if(err) log(err.message);
+    log(distance);
   });
 });
 
-
-
-/*
-
-1. Press start, log location
-2. Check accel for motion - devicemotion event
-3. Check accel for motion stop
-4. log location
-5. Get distance between locations
-6. ...
-7. Profit!
-
+/**
+ * Start the throw
+ * @return {void}
  */
+function fly(callback){
+  log("Flying!");
+  getLocation(function(err, startPosition){
+    if(err) return callback(err);
+    log('Got starting  position');
+    setTimeout(function(){
+      getLocation(function(err, endPosition){
+        if(err) return callback(err);
+        var distance = calculateDistance(
+          startPosition.coords.latitude,
+          startPosition.coords.longitude,
+          endPosition.coords.latitude,
+          endPosition.coords.longitude
+        );
+        callback(null, distance);
+      });
+    }, 5000);
+  });
+};
